b78008c507eea8fc3a5575f6806ef3bc2d2707fe
Vinicius Faria <vinicius.f4ria@gmail.com>
2023-10-20 10:44:27 -0300

    alteracoes

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Atualizacao Cyberpunk 2.0",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"cyberpunk1.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Sequencia de Lords of the Fallen lanca com problemas de desempenho",
+    "description": "bla blabla",
+    "photoCover":"lords.jpg"
+  }
+]
